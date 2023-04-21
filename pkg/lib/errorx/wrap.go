5bd79e84930b60eb5136e8d326d0787c8310a3f0
ouwenxian <ouwenxian@chinatelecom.cn>
2023-04-21 15:10:39 +0800

    support AsLast

--- pkg/lib/errorx/wrap.go
+++ pkg/lib/errorx/wrap.go
@@ -2,6 +2,7 @@ package errorx
 
 import (
 	stderrors "errors"
+	"reflect"
 )
 
 // Is reports whether any error in err's chain matches target.
@@ -34,3 +35,34 @@ func As(err error, target any) bool { return stderrors.As(err, target) }
 func Unwrap(err error) error {
 	return stderrors.Unwrap(err)
 }
+
+// AsLast finds the last error in err's chain that matches target, and if so, sets
+// target to that error value and returns true.
+func AsLast(err error, target any) bool {
+	if target == nil {
+		panic("errors: target cannot be nil")
+	}
+	val := reflect.ValueOf(target)
+	typ := val.Type()
+	if typ.Kind() != reflect.Ptr || val.IsNil() {
+		panic("errors: target must be a non-nil pointer")
+	}
+	targetType := typ.Elem()
+	if targetType.Kind() != reflect.Interface && !targetType.Implements(errorType) {
+		panic("errors: *target must be interface or implement error")
+	}
+	result := false
+	for err != nil {
+		if reflect.TypeOf(err).AssignableTo(targetType) {
+			val.Elem().Set(reflect.ValueOf(err))
+			result = true
+		}
+		if x, ok := err.(interface{ AsLast(any) bool }); ok && x.AsLast(target) {
+			return true
+		}
+		err = Unwrap(err)
+	}
+	return result
+}
+
+var errorType = reflect.TypeOf((*error)(nil)).Elem()
--- pkg/lib/errorx/wrap_test.go
+++ pkg/lib/errorx/wrap_test.go
@@ -25,3 +25,17 @@ func TestWarp(t *testing.T) {
 		t.Errorf("As(err2, &err3) got %v want: %v", As(err2, &err3), true)
 	}
 }
+
+func TestAsLast(t *testing.T) {
+	err := fmt.Errorf("first orignal error")
+	err1 := New(1, "reason1", "message1").WithCause(err)
+	err2 := fmt.Errorf("wrap2 %w", err1)
+	err3 := New(3, "reason3", "message3").WithCause(err2)
+	err4 := fmt.Errorf("wrap2 %w", err3)
+	errLast := &Error{}
+
+	if !AsLast(err4, &errLast) || errLast != err1 {
+		t.Errorf("As(err4, &errLast) got %v want: %v", AsLast(err4, &errLast), true)
+	}
+
+}
